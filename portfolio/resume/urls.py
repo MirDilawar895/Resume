f0a75dedc0ba0fe1077f7fa92015334efb8e1f42
MirDilawar895 <dilawarmughal895@gamil.com>
2022-01-04 17:19:56 +0500

    add bootstsrap template

--- portfolio/resume/urls.py
+++ portfolio/resume/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("portfolio", views.portfolio, name="portfolio"),
+    path('service', views.service, name='service'),
+    path('resume', views.resume, name='resume'),
+    path('blog', views.blog, name='blog'),
+    path('contact', views.contact, name='contact'),
+    path('project', views.project, name='project'),
+    path('content', views.content, name='content'),
+    path('alting', views.alting, name='alting'),
+    
+    
+
 ]
--- portfolio/resume/views.py
+++ portfolio/resume/views.py
@@ -4,3 +4,30 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def portfolio(request):
+    return render(request,'portfolio.html' )
+
+def service(request):
+    return render(request,'service.html' )
+
+
+def resume(request):
+    return render(request,'resume.html' )
+
+def blog(request):
+    return render(request,'blog.html' )
+
+def contact(request):
+    return render(request,'contact.html' )
+
+
+def project(request):
+    return render(request, 'project.html')
+
+def content(request):
+    return render(request, 'content.html')
+
+def alting(request):
+    return render(request, 'alting.html')
